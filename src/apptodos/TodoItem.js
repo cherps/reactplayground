4e8377786222ab6c1833bb240b9d3e4854875e98
Cherps <cherps@gmail.com>
2020-06-20 23:01:08 +0800

    Add new todo, improved delete logic

--- src/apptodos/TodoItem.js
+++ src/apptodos/TodoItem.js
@@ -2,38 +2,39 @@ import React from 'react';
 
 const TodoItem = (props) => {
 
-  const { id, title, completed, deleted, handleChange } = props;
+  const { id, title, completed, handleChange, handleDelete } = props;
   
   return (
     <div className='todo-item'>
-      <input
-        type='checkbox'
-        name='completed'
-        id={id}
-        checked={completed}
-        onChange={handleChange}
-        className='todo-checkbox'
-      />
-      <input
-        type='text'
-        name='title'
-        id={id}
-        value={title}
-        onChange={handleChange}
-        className={completed ? 'todo-title todo-title-done' : 'todo-title'}
-        />
-      
-      <label>
+      <div className='todo-checkboxtitle'>
+        <label className='todo-temp'>
+          <input
+            type='checkbox'
+            name='completed'
+            id={id}
+            checked={completed}
+            onChange={handleChange}
+            className='todo-checkbox'
+          />
+        </label>
         <input
-          type='checkbox'
-          name='deleted'
+          type='text'
+          name='title'
           id={id}
-          checked={deleted}
+          value={title}
           onChange={handleChange}
-          className='hidden'
-        />
-        <span className='material-icons todo-delete'>close</span>
-      </label>
+          className={completed ? 'todo-title todo-title-done' : 'todo-title'}
+          />
+      </div>
+      <button
+        onClick={handleDelete}
+        className='todo-delete'
+      >
+        <span
+          id={id}
+          className='material-icons'
+        >close</span>
+      </button>
     </div>
   );
 };
--- src/apptodos/TodoList.js
+++ src/apptodos/TodoList.js
@@ -13,7 +13,7 @@ const TodoList = () => {
 
     setData (prevData => {
       // Loop and look for the right object to update, using ID
-      // Update the object accordingly
+      // Update the object accordingly-- edit and delete
       const newData = prevData.map(item => {
         if (parseInt(id) === item.id) {
           type === 'checkbox' ?
@@ -26,26 +26,50 @@ const TodoList = () => {
     })
   };
 
+  // Handle Delete
+  const handleDelete = (event) => {
+    const { id } = event.target;
+    console.log('delete', id);
+
+    setData (prevData => {
+      return prevData.filter(item => item.id !== parseInt(id));
+      })
+  }
+
+  // Handle New Item
+  const handleAdd = () => {
+
+    setData (prevData => {
+      const newItemArray = [{
+          id: prevData[prevData.length-1].id + 1,
+          title: '',
+          completed: false,
+          deleted: false
+        }];
+      prevData = prevData.concat(newItemArray);
+      return prevData;
+    })
+  };
+
   // For each todo item, render a <Todo Item /> component
   const disTodoList = data.map(item => {
-    const { id, title, completed, deleted } = item;
-
-    if (!deleted) {
-      return <TodoItem
-        key={id}
-        id={id}
-        title={title}
-        completed={completed}
-        deleted ={deleted}
-        handleChange={handleChange}
-      />
-     }
-    return null;     
+    const { id, title, completed } = item;
+    return <TodoItem
+      key={id}
+      id={id}
+      title={title}
+      completed={completed}
+      handleChange={handleChange}
+      handleDelete={handleDelete}
+    />
   });
+  
+  console.log(data);
 
   return (
     <div>
       {disTodoList}
+      <button onClick={handleAdd} className='todo-add'>New Todo</button>
     </div>
   );
 };
